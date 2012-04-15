771d60b8f774482a77d60dd8559d17bd487c56e8
Heng Li <lh3@me.com>
2012-04-15 11:05:47 -0400

    bugfix: empty lines cause troubles

--- kseq.h
+++ kseq.h
@@ -186,6 +186,7 @@ typedef struct __kstring_t {
 			seq->seq.s = (char*)malloc(seq->seq.m); \
 		} \
 		while ((c = ks_getc(ks)) != -1 && c != '>' && c != '+' && c != '@') { \
+			if (c == '\n') continue; /* skip empty lines */ \
 			seq->seq.s[seq->seq.l++] = c; /* this is safe: we always have enough space for 1 char */ \
 			ks_getuntil2(ks, KS_SEP_LINE, &seq->seq, 0, 1); /* read the rest of the line */ \
 		} \
