4909a6e96c813e3308bcd32495a38ff43c104a67
Heng Li <lh3@me.com>
2013-12-12 14:44:22 -0500

    added tabtk

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 CC=gcc
 CFLAGS=-g -Wall -O2
 
+all:seqtk tabtk
+
 seqtk:seqtk.c khash.h kseq.h
 		$(CC) $(CFLAGS) seqtk.c -o $@ -lz -lm
 
+tabtk:tabtk.c kseq.h kstring.h
+		$(CC) $(CFLAGS) tabtk.c -o $@ -lz -lm
+
 clean:
-		rm -fr gmon.out *.o ext/*.o a.out seqtk *~ *.a *.dSYM session*
+		rm -fr gmon.out *.o ext/*.o a.out seqtk tabtk *~ *.a *.dSYM session*
---
+++ tabtk.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <zlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <math.h>
+#include "kvec.h"
+#include "kstring.h"
+
+#include "kseq.h"
+KSTREAM_INIT(gzFile, gzread, 65536)
+
+typedef kvec_t(uint32_t) vec32_t;
+typedef kvec_t(uint64_t) vec64_t;
+
+int main_cut(int argc, char *argv[])
+{
+	vec64_t cols = {0,0,0}, buf = {0,0,0};
+	gzFile fp;
+	kstream_t *ks;
+	kstring_t str = {0,0,0}, out = {0,0,0};
+	int dret, sep = '\t', c, reorder = 0;
+
+	while ((c = getopt(argc, argv, "rd:f:")) >= 0) {
+		if (c == 'r') reorder = 1;
+		else if (c == 'd') sep = optarg[0];
+		else if (c == 'f') {
+			int32_t beg, end, x;
+			char *p = optarg;
+			while ((x = strtol(p, &p, 10)) > 0) {
+				beg = end = x;
+				if (*p == '-')
+					end = (x = strtol(p + 1, &p, 10)) >= beg? x : INT32_MAX;
+				kv_push(uint64_t, cols, (uint64_t)(beg-1)<<32 | end);
+				if (*p) ++p; // skip ','
+				else break;
+			}
+			if (*p) {
+				fprintf(stderr, "[E::%s] malformated field string. Abort!\n", __func__);
+				return 1;
+			}
+		}
+	}
+	
+	if (argc == optind && isatty(fileno(stdin))) {
+		fprintf(stderr, "Usage: tabtk cut [-r] [-d sep] [-f fields] [file.txt]\n");
+		return 1;
+	}
+
+	if (cols.n == 0) {
+		fprintf(stderr, "[E::%s] no list of fields is specified.\n", __func__);
+		return 2;
+	}
+
+	if (!reorder) { // insertion sort
+		uint64_t *i;
+		for (i = cols.a + 1; i < cols.a + cols.n; ++i)
+			if (*i < *(i - 1)) {
+				uint64_t *j, tmp = *i;
+				for (j = i; j > cols.a && tmp < *(j-1); --j) *j = *(j - 1);
+				*j = tmp;
+			}
+	}
+
+	fp = (optind == argc && !isatty(fileno(stdin))) || strcmp(argv[optind], "-") == 0? gzdopen(fileno(stdin), "r") : gzopen(argv[optind], "r");
+	ks = ks_init(fp);
+	while (ks_getuntil2(ks, KS_SEP_LINE, &str, &dret, 0) >= 0) {
+		int b, i;
+		buf.n = 0; out.l = 0;
+		for (i = b = 0; i <= str.l; ++i) {
+			if (str.s[i] == sep || i == str.l) {
+				kv_push(uint64_t, buf, (uint64_t)b<<32 | i);
+				b = i + 1;
+			}
+		}
+		for (i = 0; i < cols.n; ++i) {
+			int32_t j, beg = cols.a[i]>>32, end = (int32_t)cols.a[i];
+			for (j = beg; j < end && j < buf.n; ++j) {
+				uint64_t x = buf.a[j];
+				if (out.l) kputc('\t', &out);
+				kputsn(&str.s[x>>32], (uint32_t)x - (x>>32), &out);
+			}
+		}
+		puts(out.s);
+	}
+	ks_destroy(ks);
+	gzclose(fp);
+
+	free(str.s); free(out.s); free(cols.a); free(buf.a);
+	return 0;
+}
+
+static int usage()
+{
+	fprintf(stderr, "\n");
+	fprintf(stderr, "Usage:   tabtk <command> [arguments]\n");
+	fprintf(stderr, "Version: 0.0\n\n");
+	fprintf(stderr, "Command: cut       Unix cut with optional column reordering\n");
+	fprintf(stderr, "\n");
+	return 1;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1) return usage();
+	if (strcmp(argv[1], "cut") == 0) main_cut(argc-1, argv+1);
+	else {
+		fprintf(stderr, "[main] unrecognized commad '%s'. Abort!\n", argv[1]);
+		return 1;
+	}
+	return 0;
+}
