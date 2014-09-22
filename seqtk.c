77bc4e717cc4def2611c96f549ed7d7f208e4f6a
Daniel Nicorici <Daniel.Nicorici@gmail.com>
2014-09-22 08:39:43 +0300

    Update seqtk.c
    updated version string to 1.0-r68a-dirty

--- seqtk.c
+++ seqtk.c
@@ -1360,7 +1360,7 @@ static int usage()
 {
 	fprintf(stderr, "\n");
 	fprintf(stderr, "Usage:   seqtk <command> <arguments>\n");
-	fprintf(stderr, "Version: 1.0-r68-dirty\n\n");
+	fprintf(stderr, "Version: 1.0-r68a-dirty\n\n");
 	fprintf(stderr, "Command: seq       common transformation of FASTA/Q\n");
 	fprintf(stderr, "         comp      get the nucleotide composition of FASTA/Q\n");
 	fprintf(stderr, "         sample    subsample sequences\n");
