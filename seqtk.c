2ac30555a8501e43f42d607cff829bc48f28c8e7
Heng Li <lh3@me.com>
2016-04-06 10:01:50 -0400

    Release seqtk-1.1-r91

--- seqtk.c
+++ seqtk.c
@@ -1558,7 +1558,7 @@ static int usage()
 {
 	fprintf(stderr, "\n");
 	fprintf(stderr, "Usage:   seqtk <command> <arguments>\n");
-	fprintf(stderr, "Version: 1.0-r87-dirty\n\n");
+	fprintf(stderr, "Version: 1.1-r91\n\n");
 	fprintf(stderr, "Command: seq       common transformation of FASTA/Q\n");
 	fprintf(stderr, "         comp      get the nucleotide composition of FASTA/Q\n");
 	fprintf(stderr, "         sample    subsample sequences\n");
