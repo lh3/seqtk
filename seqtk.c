96eb70eabc29bc1a221b2c07c4b265f84a449fb0
Wenfei Xian <2316469036@qq.com>
2023-10-20 19:35:20 +0200

    Add files via upload

--- seqtk.c
+++ seqtk.c
@@ -2012,6 +2012,8 @@ int stk_telo(int argc, char *argv[])
 		if (max >= min_score) {
 			if (!show_profile) printf(">%s\t0\t%ld\t%ld\n", seq->name.s, max_i + 1, seq->seq.l);
 			printf("%s\n", seq->seq.s);
+			if (!show_profile) printf(">%s_remove\t%ld\t%ld\t%ld\n", seq->name.s, max_i, seq->seq.l, seq->seq.l);
+			printf("%s\n", seq->seq.s + max_i + 1);
 			sum_telo += max_i + 1;
 			st = max_i + 1;
 		}
@@ -2030,7 +2032,12 @@ int stk_telo(int argc, char *argv[])
 		}
 		if (max >= min_score) {
 			if (!show_profile) printf(">%s\t%ld\t%ld\t%ld\n", seq->name.s, max_i, seq->seq.l, seq->seq.l);
-			printf("%s\n", seq->seq.s); 
+			printf("%s\n", seq->seq.s);
+			if (!show_profile) printf(">%s_remove\t%ld\t%ld\t%ld\n", seq->name.s, max_i, seq->seq.l, seq->seq.l);
+			char temp_char = seq->seq.s[max_i];
+			seq->seq.s[max_i] = '\0';
+			printf("%s\n", seq->seq.s);
+			seq->seq.s[max_i] = temp_char;
 			sum_telo += seq->seq.l - max_i;
 		}
 	}
